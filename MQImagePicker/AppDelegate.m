baea4b22f1ccde8914479a9e1063a83cd31a485d
Mubeen Raza Qazi <>
2019-06-14 15:06:18 +0500

    GMImagePicker Implmentation Added

--- MQImagePicker/AppDelegate.m
+++ MQImagePicker/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc]init];
+    [self.window makeKeyAndVisible];
+    self.window.rootViewController = [[ViewController alloc]init];
+    
     return YES;
 }
 
--- MQImagePicker/ViewController.m
+++ MQImagePicker/ViewController.m
@@ -8,9 +8,11 @@
 
 #import "ViewController.h"
 #import <UIKit/UIKit.h>
+#import "GMImagePickerController.h"
 
-@interface ViewController (){
+@interface ViewController ()<GMImagePickerControllerDelegate>{
     UIButton *showGalleryBtn;
+    GMImagePickerController *picker;
 }
 
 @end
@@ -19,6 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
     [self setupButton];
 }
 
@@ -26,18 +29,27 @@
 - (void)setupButton{
     showGalleryBtn = [[UIButton alloc]init];
     [showGalleryBtn setTitle:@"Show Gallery" forState:UIControlStateNormal];
-    [showGalleryBtn setTitleColor:[UIColor cyanColor] forState:UIControlStateNormal];
+    [showGalleryBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     showGalleryBtn.translatesAutoresizingMaskIntoConstraints = false;
+    [showGalleryBtn addTarget:self action:@selector(openGMPicker) forControlEvents:UIControlEventTouchUpInside];
     
     [self.view addSubview:showGalleryBtn];
     
     [showGalleryBtn.heightAnchor constraintEqualToConstant:40].active = YES;
-    //[showGalleryBtn.widthAnchor constraintEqualToConstant:160].active = YES;
-    //[showGalleryBtn.topAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
     [showGalleryBtn.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
     [showGalleryBtn.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
-    
-    
+}
+
+- (void)openGMPicker{
+    picker = [[GMImagePickerController alloc] init];
+    picker.delegate = self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+
+#pragma mark - GMImagePicker Delegate
+-(void)assetsPickerController:(GMImagePickerController *)picker didFinishPickingAssets:(NSArray *)assets{
+    [picker.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 
