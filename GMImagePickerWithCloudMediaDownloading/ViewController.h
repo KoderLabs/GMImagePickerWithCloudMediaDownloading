fad64d4803f097f743b617cc955012697b23a399
Mubeen Raza Qazi <>
2019-06-14 15:28:35 +0500

    Renaming Project

---
+++ GMImagePickerWithCloudMediaDownloading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GMImagePickerWithCloudMediaDownloading
+//
+//  Created by Mubeen Raza Qazi on 14/06/2019.
+//  Copyright © 2019 Mubeen Raza Qazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GMImagePickerWithCloudMediaDownloading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GMImagePickerWithCloudMediaDownloading
+//
+//  Created by Mubeen Raza Qazi on 14/06/2019.
+//  Copyright © 2019 Mubeen Raza Qazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
